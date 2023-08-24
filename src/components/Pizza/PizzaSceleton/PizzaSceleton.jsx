7af95d4b5c185233c85124b46bc370980ce47e0a
Dima Ryabov <dima.poltergeist.2015@yandex.ru>
2023-08-24 19:36:51 +0300

    добавил react-sceleton для того чтобы пользователь видел, что идет загрузка с использованием библиотеки react-content-loader, подогнал под адаптив и отображение определенного количества элементов при разном разрешении экрана

---
+++ src/components/Pizza/PizzaSceleton/PizzaSceleton.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import ContentLoader from "react-content-loader";
+import styles from "./PizzaSceleton.module.scss";
+
+const MyLoader = (props) => (
+  <>
+    <div className={styles.main}>
+      <ContentLoader
+        className={styles.main}
+        speed={1}
+        viewBox="0 0 257 432"
+        backgroundColor="#f3f3f3"
+        foregroundColor="#ecebeb"
+      >
+        <rect x="1" y="367" rx="17" ry="17" width="227" height="45" />
+        <rect x="0" y="263" rx="17" ry="17" width="227" height="82" />
+        <rect x="0" y="206" rx="17" ry="17" width="227" height="40" />
+        <circle cx="110" cy="97" r="95" />
+      </ContentLoader>
+    </div>
+    <div className={styles.root}>
+      <ContentLoader
+        className={styles.root}
+        speed={1}
+        viewBox="0 0 455 610"
+        backgroundColor="#f3f3f3"
+        foregroundColor="#ecebeb"
+        {...props}
+      >
+        <rect x="0" y="448" rx="9" ry="9" width="100%" height="82" />
+        <rect x="2" y="554" rx="9" ry="9" width="100%" height="39" />
+        <rect x="6" y="375" rx="9" ry="9" width="100%" height="40" />
+        <circle cx="222" cy="187" r="166" />
+      </ContentLoader>
+    </div>
+  </>
+);
+
+export default MyLoader;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,9 +3,11 @@ import React, { useState, useEffect } from "react";
 import Categories from "../components/Categories/Categories";
 import Sort from "../components/Sort/Sort";
 import Pizza from "../components/Pizza/Pizza";
+import PizzaSceleton from "../components/Pizza/PizzaSceleton/PizzaSceleton";
 
 function Home() {
   const [pizzas, setPizzas] = useState(null);
+  const [width, setWidth] = useState(window.innerWidth);
 
   async function getPizzas() {
     let res = await fetch("https://64e1008250713530432ce0c5.mockapi.io/pizzas");
@@ -17,6 +19,34 @@ function Home() {
     getPizzas();
   }, []);
 
+  function array() {
+    if (width < 1469 && width > 1169) {
+      return [...new Array(8)];
+    }
+    if (width < 1170 && width > 864) {
+      return [...new Array(6)];
+    }
+    if (width < 865 && width > 561) {
+      return [...new Array(4)];
+    }
+    if (width < 562 && width > 0) {
+      return [...new Array(2)];
+    } else {
+      return [...new Array(10)];
+    }
+  }
+
+  useEffect(() => {
+    const handleResize = (event) => {
+      setWidth(event.target.innerWidth);
+      array();
+    };
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <>
       <h2 className="content__title">Все пиццы</h2>
@@ -25,11 +55,9 @@ function Home() {
         <Sort />
       </div>
       <div className="content__items">
-        {pizzas ? (
-          pizzas.map((pizza) => <Pizza key={pizza.id} {...pizza} />)
-        ) : (
-          <h3>Загрузка пицц...</h3>
-        )}
+        {pizzas
+          ? pizzas.map((pizza) => <Pizza key={pizza.id} {...pizza} />)
+          : array().map((_, index) => <PizzaSceleton key={index} />)}
       </div>
     </>
   );
