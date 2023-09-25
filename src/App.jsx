7447c9978b2b72c896fc465eabdda2506bf44acd
Dima Ryabov <dima.poltergeist.2015@yandex.ru>
2023-09-25 15:55:07 +0300

    при поиске пицц, если список пуст появляется таких пицц нет

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState } from "react";
+import React, { createContext } from "react";
 import { Routes, Route } from "react-router-dom";
 
 import Header from "./components/Header/Header";
